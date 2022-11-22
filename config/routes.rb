2315aa53a081959227480f503a8f02f7b9a8c1cb
kermyndak <kermyndak@bk.ru>
2022-11-22 13:21:58 +0300

    Added controller Root and two views: input, show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'root/input'
+  get 'root/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
