0f79a5eecaf4779e455b77a38d87f633237d0053
kermyndak <kermyndak@bk.ru>
2022-12-03 15:35:18 +0300

    Change tests

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -9,58 +9,39 @@ RSpec.describe "Roots", type: :request do
     end
   end
 
-  describe "GET /show" do
+  describe "POST /show" do
     it "returns http success" do
-      get root_show_path, params: { query: 350 }
+      post '/root/show', params: { query: 350 }, xhr: true
       expect(response).to have_http_status(:success)
     end
 
-    context 'returns http code 302' do
-      it "empty parameter" do
-        get root_show_path, params: {}
-        expect(response).to have_http_status(302)
-      end
-
+    context 'returns http code 200 with bad input' do
       it "Bad input" do
-        get root_show_path, params: { query: 'qwerty' }
-        expect(response).to have_http_status(302)
+        post '/root/show', params: { query: 'qwerty' }, xhr: true
+        expect(response).to have_http_status(200)
       end
     end
 
     context 'controller tests' do
       it 'test @arr' do
-        get root_show_path, params: { query: 350 }
-        expect(assigns(:arr)).to eq([[1, 1], [6, 6], [28, 28], [220, 284]])
+        post "/root/show", params: { query: 350 }, xhr: true
+        expect(assigns(:arr)).to eq([[220, 284]])
       end
 
       it 'test @number' do
-        get root_show_path, params: { query: 350 }
+        post '/root/show', params: { query: 350 }, xhr: true
         expect(assigns(:number)).to eq(350)
-      end
-    end
-
-    context 'redirect to input' do
-      it 'redirect with bad input' do
-        get root_show_path, params: { query: 'qwerty' }
-        expect(response).to redirect_to(root_path)
-      end
-
-      it 'redirect with empty input' do
-        get root_show_path, params: {}
-        expect(response).to redirect_to(root_path)
       end
     end
 
     context "parse answer" do
       it 'parameter 350' do
-        get root_show_path, params: { query: 350 }
+        post '/root/show', params: { query: 350 }, xhr: true
         html = Nokogiri::HTML(response.body)
-        answer = [[1, 1], [6, 6], [28, 28], [220, 284]]
+        answer = [[220, 284]]
         td = html.search('td')
-        answer.each_with_index do |pair, index|
-          exp = [td[2 * index].text.to_i, td[1 + 2 * index].text.to_i]
-          expect(exp).to eq(pair)
-        end
+        exp = [td[0].text.to_i, td[1].text.to_i]
+        expect(exp).to eq([220, 284])
       end
     end
   end
